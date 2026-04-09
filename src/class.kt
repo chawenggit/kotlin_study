41164172d537895f1ae02aa05f3b9064f3cbd4ed
chaweng_thinkpad <chaweng69@gmail.com>
2026-04-09 08:09:02 +0700

    data class

--- src/class.kt
+++ src/class.kt
@@ -13,7 +13,14 @@ class Contact1(val id: Int, var email: String = "example@gmail.com") {
 
 //Member function
 //Data classes
+data class User(val name: String, val id: Int)
 
+val user = User("Alex", 1)
+val secondUser = User("Alex", 2)
+val thirdUser = User("Max", 3)
+
+// Automatically uses toString() function so that output is easy to read
+// User(name=Alex, id=1)
 //Create instance
 val contact = Contact(1, "mary@gmail.com")
 fun main() {
@@ -21,4 +28,36 @@ fun main() {
     println(contact.category)
     contact.email = "jane@gmail.com"
     println(contact.email)
+//    data class
+    println(user)
+    println("user == secondUser: ${user == secondUser}")
+    println("user == thirdUser: ${user == thirdUser}")
+
+    println(user.copy())
+    println(user.copy("Max"))
+    println(user.copy(id = 3))
+
+    //*** Exercise 1
+    data class Employee(val name: String, var salary: Int)
+
+    val emp = Employee("Mary", 20)
+    println(emp)
+    emp.salary += 10
+    println(emp)
+    //*** Exercise 2
+
+    // Write your code here
+// data class Name(...)
+    data class City(val name: String, val country: String)
+    data class Name(val firstName: String, val lastName: String)
+    data class Address(val street: String, val city: City)
+    data class Person(val name: Name, val address: Address, val ownsAPet: Boolean = true)
+
+    val person = Person(
+        Name("John", "Smith"),
+        Address("123 Fake Street", City("Springfield", "US")),
+        ownsAPet = false
+    )
+    println(person)
+    //*** Exercise 3
 }
