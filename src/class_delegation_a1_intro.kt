236f94d80f7fdee2aa4ebae9f8b99c2542589009
chaweng_ofc <chaweng69@gmail.com>
2026-06-04 14:03:11 +0700

    add  class_delegation_a1_intro.kt and class_delegation_a2_issue.kt

---
+++ src/class_delegation_a1_intro.kt
@@ -0,0 +1,31 @@
+package class_delegation_a1_intro
+/*
+    Chef and Waiter have the same fun prepareEntree so next interfaces come in
+ */
+class Chef {
+    fun prepareEntree(name: String): Entree? = when (name) {
+        "Tossed Salad"
+            -> Entree.TOSSED_SALAD
+        "Salmon on Rice" -> Entree.SALMON_ON_RICE
+        else
+            -> null
+    }
+}
+class Waiter(private val chef: Chef) {
+    // The waiter can prepare a beverage by himself...
+    fun prepareBeverage(name: String): Beverage? = when (name) {
+        "Water" -> Beverage.WATER
+        "Soda"-> Beverage.SODA
+        else-> null
+    }
+    // ... but needs the chef to prepare an entree
+    fun prepareEntree(name: String): Entree? = chef.prepareEntree(name)
+    fun acceptPayment(money: Int) = println("Thank you for paying for your meal")
+}
+
+enum class Entree { TOSSED_SALAD, SALMON_ON_RICE }
+enum class Beverage { WATER, SODA }
+
+val waiter = Waiter(Chef())
+val beverage = waiter.prepareBeverage("Soda")
+val entree = waiter.prepareEntree("Salmon on Rice")
---
+++ src/class_delegation_a2_issue.kt
@@ -0,0 +1,38 @@
+package class_delegation_a2_issue
+/*
+    Chef and Waiter have the same fun prepareEntree so next interfaces come in
+    Problem interface does not help boilerplate
+ */
+
+interface KitchenService {
+    fun prepareEntree(name: String): Entree?
+}
+
+class Chef :KichenService {
+    override fun prepareEntree(name: String): Entree? = when (name) {
+        "Tossed Salad"
+            -> Entree.TOSSED_SALAD
+        "Salmon on Rice" -> Entree.SALMON_ON_RICE
+        else
+            -> null
+    }
+}
+
+class Waiter(private val chef: Chef) :KichenService {
+    // The waiter can prepare a beverage by himself...
+    fun prepareBeverage(name: String): Beverage? = when (name) {
+        "Water" -> Beverage.WATER
+        "Soda"-> Beverage.SODA
+        else-> null
+    }
+    fun acceptPayment(money: Int) = println("Thank you for paying for your meal")
+    // ... but needs the chef to prepare an entree
+    override fun prepareEntree(name: String): Entree? = chef.prepareEntree(name)
+}
+
+enum class Entree { TOSSED_SALAD, SALMON_ON_RICE }
+enum class Beverage { WATER, SODA }
+
+val waiter = Waiter(Chef())
+val beverage = waiter.prepareBeverage("Soda")
+val entree = waiter.prepareEntree("Salmon on Rice")
