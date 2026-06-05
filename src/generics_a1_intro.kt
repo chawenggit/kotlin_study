74b46689dae0171853b3a7f0d114d1b58474dd5b
chaweng_thinkpad <chaweng69@gmail.com>
2026-06-05 15:59:21 +0700

    generics_a1_intro

---
+++ src/generics_a1_intro.kt
@@ -0,0 +1,23 @@
+package generics_a1_intro
+// FIRST DAY
+enum class Coffee { LIGHT_ROAST, MEDIUM_ROAST, DARK_ROAST }
+class Mug(val beverage: Coffee)
+fun drink(coffee: Coffee) = println("Drinking coffee: $coffee")
+
+// NEXT DAY
+enum class Tea { GREEN_TEA, BLACK_TEA, RED_TEA }
+class TeaMug(val tea: Tea)
+   // change name
+class CoffeeMug(val beverage: Coffee)
+    // override drink
+fun drink(tea: Tea) = println("Drinking tea: $tea")
+
+//ISSUES  NEXT WEEK expand more beverages it must create new more CLASS
+
+fun main() {
+    val mug = Mug(Coffee.MEDIUM_ROAST)
+    drink(coffee = Coffee.LIGHT_ROAST)
+    drink(mug.beverage)
+//   next day
+    drink(Tea.GREEN_TEA)
+}
