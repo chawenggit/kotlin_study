fa081b8b8d5faf9e21d69060714846876337e0f7
chaweng_ofc <chaweng69@gmail.com>
2026-06-08 17:00:19 +0700

    collection_maps_a1 start

---
+++ src/collection_maps_a1.kt
@@ -0,0 +1,14 @@
+package collection_maps_a1
+
+/*
+    book Kotlin illustrated Guide
+    Kotlin Map = Python dict — same concept, different syntax.
+ */
+
+fun main() {
+    // ------ simple Pair Class
+    val association = Pair("Nail", "Hammer")
+    println(association.first) // Nail
+    println(association.second) // Hammer
+
+}
